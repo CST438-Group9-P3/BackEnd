cb3d23084b8991d27fef5f4fe6587e8f5a26ea9e
Kevinlzs <kevinlzs.04@gmail.com>
2024-11-03 18:37:05 -0800

    Connected springboot app with mariadb and added user entity allong with it's controller and repository

--- project3Group9/src/main/java/com/example/project3group9/Project3Group9Application.java
+++ project3Group9/src/main/java/com/example/project3group9/Project3Group9Application.java
@@ -1,6 +1,7 @@
 package com.example.project3group9;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ project3Group9/src/main/java/com/example/project3group9/api/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.project3group9.api.controller;
+
+import com.example.project3group9.api.model.User;
+import com.example.project3group9.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> users(){
+        return userRepository.findAll();
+    }
+}

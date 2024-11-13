46272762d66e53040738f648eec7a6b1d4fc3249
cs-mg-gh <145803911+cs-mg-gh@users.noreply.github.com>
2024-11-13 11:30:29 -0800

    added getters and setters for OttersMenBasketBall.java

--- src/main/java/com/example/project3group9/api/model/OttersMenBasketBall.java
+++ src/main/java/com/example/project3group9/api/model/OttersMenBasketBall.java
@@ -39,4 +39,17 @@ public class OttersMenBasketBall {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getTeam() {
+        return team;
+    }
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }

9683572f7f82ffd0fd665f8a2480c10227319e71
Kevinlzs <kevinlzs.04@gmail.com>
2024-11-18 15:23:01 -0800

    added login route and delete account route

--- src/main/java/com/example/project3group9/api/controller/UserController.java
+++ src/main/java/com/example/project3group9/api/controller/UserController.java
@@ -4,10 +4,11 @@ import com.example.project3group9.api.model.User;
 import com.example.project3group9.api.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -22,5 +23,30 @@ public class UserController {
     @GetMapping("/users")
     public List<User> users(){
         return userRepository.findAll();
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestParam String email, @RequestParam String username) {
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        } else {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setUsername(username);
+            newUser.setAccountBalance(0.0); // default value
+            return userRepository.save(newUser);
+        }
+    }
+
+    @DeleteMapping("/deleteAcc")
+    public ResponseEntity<String> deleteAccount(@RequestParam String email, @RequestParam String username) {
+        Optional<User> userOptional = userRepository.findByEmailAndUsername(email, username);
+        if (userOptional.isPresent()) {
+            userRepository.delete(userOptional.get());
+            return ResponseEntity.ok("User successfully deleted");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }

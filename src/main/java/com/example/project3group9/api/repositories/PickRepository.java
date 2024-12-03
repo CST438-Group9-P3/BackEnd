f5976e73d3861a2315b7820a1f524fdc2442a919
Kevinlzs <kevinlzs.04@gmail.com>
2024-12-02 20:48:53 -0800

    added pick model, updated player, created createpick route so front end can start creating picks

--- src/main/java/com/example/project3group9/api/repositories/PickRepository.java
+++ src/main/java/com/example/project3group9/api/repositories/PickRepository.java
@@ -1,12 +1,10 @@
 package com.example.project3group9.api.repositories;
 
+import com.example.project3group9.api.model.Pick;
 import com.example.project3group9.api.model.Players;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PlayersControllerRepository extends JpaRepository<Players, Integer>{
-    Optional<Players> findByPlayerId(Integer playerId);
+public interface PickRepository extends JpaRepository<Pick, Integer> {
 }
